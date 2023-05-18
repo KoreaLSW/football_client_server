34dcdf3c90e659b37a2e206846567b35b31961fd
KoreaLSW <89916970+KoreaLSW@users.noreply.github.com>
2023-05-19 01:40:02 +0900

    Add files via upload

--- web.js
+++ web.js
@@ -737,13 +737,15 @@ function date() {
 }
 
 async function ogsCall(options) {
+    console.log('뉴스옵션:', data);
     try {
         return await ogs(options).then((data) => {
+            console.log('뉴스데이타!!!@@:', data);
             const { error, result, response } = data;
             // console.log('error:', error); // This returns true or false. True if there was an error. The error itself is inside the results object.
             // console.log('result:', result); // This contains all of the Open Graph results
             // console.log('response:', response); // This contains the HTML of page
-            console.log('뉴스사진:', result);
+
             return result.ogImage.url;
         });
     } catch (error) {
